4effb5c64b6ce68bae3475196bb3b853c0d57a1e
salmxnz <138376829+salmxnz@users.noreply.github.com>
2023-07-05 21:11:50 +0400

    Commit2

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main(){
+  string name;
+  cout<< "Enter name: ";
+  cin << name;
+  cout << "Hello my name is: " << name;
+  return 0;
+}
